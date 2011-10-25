89075209236579d51a4c9ce2e8744c37098c4ee3
Luke Amdor <luke.amdor@gmail.com>
2011-10-24 23:40:01 -0500

    normal credit of ex1 DONE

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -17,11 +17,8 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
-
-
-
-
+    predictions = X * theta;
+    theta = theta - (alpha / m) * (X' * (predictions - y));
 
     % ============================================================
 

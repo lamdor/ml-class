79ad17124a8675d5cb6e8b4f3a2d42cd44388232
Luke Amdor <luke.amdor@t8webware.com>
2011-11-06 08:23:41 -0600

    this should have been subtracted

--- octave/ex2/costFunctionReg.m
+++ octave/ex2/costFunctionReg.m
@@ -2,12 +2,12 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
 %   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+%   gradient of the cost w.r.t. to the parameters.
 
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -26,8 +26,8 @@ predictions = sigmoid(X * theta);
 
 J = (-1 / m) * ( y' * log(predictions) +
                 (1 - y)' * log(1 - predictions)) + (lambda / (2 * m)) * sum(theta(2:n) .^ 2);
-    
-grad = (1 / m) * (X' * (predictions - y) +
+
+grad = (1 / m) * (X' * (predictions - y) -
                   lambda * (eyeWithoutTheta0 * theta));
 
 % =============================================================
